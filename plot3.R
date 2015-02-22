635f886534d0f55736f232a4c39957cd2057020e
Aasish Agarwal <aasish.agarwal.75@gmail.com>
2015-02-22 12:32:11 +0530

    plots

--- plot3.R
+++ plot3.R
@@ -7,59 +7,39 @@
 
 ##############################################################################
 # DOWNLOAD ,UNCOMPRESS, LOAD Data
-
-# File "FNEI_data.zip" is downloaded only once
-# If for some reason file must be downloaded again, manualy remove the existing file from your working directory.
-
-# Download And Unzip Data File
 url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
 projectDatafile <- "FNEI_data.zip"
-
-cat("\nDownloadoing data set if not already exists: " , projectDatafile)
 if (! file.exists(projectDatafile)){
-  cat("\nStaring data download")
   download.file(url , projectDatafile, method = "curl")
   unzip(projectDatafile)
-} else {
-  cat("\nFile already downloaded")
 } 
-
-# Load data sets if not already loaded
-cat("\nLoading data sets if not already loaded")
 if (!exists("NEI")) {
   NEI <- readRDS("summarySCC_PM25.rds")
   SCC <- readRDS("Source_Classification_Code.rds")
 }
-cat("\nDone with data load")
 
 ##############################################################################
 # Agreegate total emissions from PM2.5 for fips "24510" agreegated by source type
 # Total emissions are recorded in variable total_emissions 
-
 library(dplyr)
+library(ggplot2)
 NEI_group_by_type <- NEI %>%
   tbl_df() %>%
   group_by(type,year) %>%
   filter(fips == "24510") %>%
   summarize(total_emissions = sum(Emissions) )  
 
-
 ##############################################################################
 # CREATE PNG File
-
 g <- ggplot(NEI_group_by_type, aes(year,total_emissions ))
-p <- g + geom_point() + 
+p <- g + geom_point(aes(colour = type)) + geom_line() +
   facet_grid(. ~ type) + 
   facet_wrap( ~ type, nrow = 2, ncol = 2) + 
+  guides(colour=FALSE) + 
   labs(y = "Total Emissions (Tons)") +
   ggtitle("Emissions By Type - Baltimore")
-
 print(p)
-
 dev.copy(png,file = "plot3.png", width = 480, height = 480, units = "px")
-
 # Winding up by closing png device
 dev.off()
-
-cat("\nCreated plot3.png in your working directory")
 
