dc1891f9544ff99f2d68c7ed009740cf4ecd7687
Aasish Agarwal <aasish.agarwal.75@gmail.com>
2015-02-22 13:35:30 +0530

    plots

--- plot6.R
+++ plot6.R
@@ -1,27 +1,13 @@
-# Compare emissions from motor vehicle sources in Baltimore City with emissions 
-# from motor vehicle sources in Los Angeles County, California (fips == 06037). 
-# Which city has seen greater changes over time in motor vehicle emissions?
-
 url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
 projectDatafile <- "FNEI_data.zip"
-
-cat("\nDownloadoing data set if not already exists: " , projectDatafile)
 if (! file.exists(projectDatafile)){
-  cat("\nStaring data download")
   download.file(url , projectDatafile, method = "curl")
   unzip(projectDatafile)
-} else {
-  cat("\nFile already downloaded")
 } 
-
-# Load data sets if not already loaded
-cat("\nLoading data sets if not already loaded")
 if (!exists("NEI")) {
   NEI <- readRDS("summarySCC_PM25.rds")
   SCC <- readRDS("Source_Classification_Code.rds")
 }
-cat("\nDone with data load")
-
 ##############################################################################
 # Agreegate total emissions from PM2.5 agreegated by fuel (SCC)
 # Total emissions in 1000's are recorded in variable total_emissions 
@@ -47,21 +33,13 @@ NEI_group_by_fuel <- NEI_group_by_fuel %>%
   summarize(total_emissions = sum(total_emissions) )  
 
 ##############################################################################
-# CREATE PNG File
-
 g <- ggplot(NEI_group_by_fuel, aes(year,total_emissions ))
-p <- g + geom_point(aes(color = City), size=5, shape=21) + 
+p <- g + geom_point(aes(color = City), size=5, shape=18) + 
+  geom_line(aes(color = City)) +
   facet_grid( EI.Sector ~ ., scales="free", as.table=F) +
   labs(y = "Total Emissions (Tons)") +
   ggtitle("Emissions By Fuel Type")
 
 print(p)
-
 dev.copy(png,file = "plot6.png", width = 680, height = 1080, units = "px")
-
-# Winding up by closing png device
 dev.off()
-
-cat("\nCreated plot6.png in your working directory")
-
-
