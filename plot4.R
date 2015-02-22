fc3f3703f34554bac438abdadd8c3840e27159c0
Aasish Agarwal <aasish.agarwal.75@gmail.com>
2015-02-22 12:46:01 +0530

    plots

--- plot4.R
+++ plot4.R
@@ -1,23 +1,15 @@
-# Download And Unzip Data File
 url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
 projectDatafile <- "FNEI_data.zip"
-
-cat("\nDownloadoing data set if not already exists: " , projectDatafile)
 if (! file.exists(projectDatafile)){
   cat("\nStaring data download")
   download.file(url , projectDatafile, method = "curl")
   unzip(projectDatafile)
-} else {
-  cat("\nFile already downloaded")
 } 
 
-# Load data sets if not already loaded
-cat("\nLoading data sets if not already loaded")
 if (!exists("NEI")) {
   NEI <- readRDS("summarySCC_PM25.rds")
   SCC <- readRDS("Source_Classification_Code.rds")
 }
-cat("\nDone with data load")
 
 ##############################################################################
 # Agreegate total emissions from PM2.5 agreegated by fuel (SCC)
@@ -42,7 +34,9 @@ NEI_group_by_fuel <- NEI_group_by_fuel %>%
 # CREATE PNG File
 
 g <- ggplot(NEI_group_by_fuel, aes(year,total_emissions ))
-p <- g + geom_point() + 
+p <- g + geom_point(aes(size=year,color=EI.Sector)) + geom_line() +
+  guides(colour=FALSE) +
+  guides(size=FALSE) +
   facet_grid( EI.Sector ~ ., scales="free") +
   labs(y = "Total Emissions (1000 Tons)") +
   ggtitle("Emissions By Fuel Type")
